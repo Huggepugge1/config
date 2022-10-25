17ede4adc069941fe88195701118252269c41302
Hugo Lindström <huggepugge1@gmail.com>
2022-10-25 13:25:10 +0200

    vimsetup

--- vimsetup.sh
+++ vimsetup.sh
@@ -2,7 +2,13 @@
 
 home=/home/$USER
 
+# Make files
 cp ./files/.vimrc $home/.vimrc
 mkdir $home/.vim
 mkdir $home/.vim/autoload $home/.vim/backup $home/.vim/colors $home/.vim/plugged
+
+# Get plugin manager
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Install plugins
 vim +PlugInstall +qall
